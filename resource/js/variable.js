b34a8ffb79375942fda613e72e82729babb690ab
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 20:21:25 +0800

    Update variable.js

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -5,7 +5,7 @@ alert("testing");
 
   $(document).ready(function() {
             var nm;
-    $("#remote_iframe_0").height(600);
+    $("remote_iframe_0").height(600);
             $.ajax({
                 type: "GET" ,
                 url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
