c1b8baca03a0233c6daef70d43be1b7d17b19c07
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 19:40:21 +0800

    Update variable.js

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,6 +1,6 @@
   
   var varRoleDetailPDFlocation ;
-alert("Varaible js File loading");
+alert("Varaible js File loading 123");
   $(document).ready(function() {
 				$.get('config/config.xml', function(d){
 					$(d).find('configurationLink').each(function(){
