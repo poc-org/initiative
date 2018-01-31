2bf0936c25789afd90a88ca54eb4a2ede3e16968
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 19:48:15 +0800

    testing

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -2,7 +2,7 @@
   var varRoleDetailPDFlocation ;
 alert("Varaible js File loading Shakeel");
   $(document).ready(function() {
-				$.get('https://raw.githubusercontent.com/poc-org/initiative/itg/resource/js/variable.js', function(d){
+				$.get('https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml', function(d){
 					$(d).find('configurationLink').each(function(){
 							var $configurationLink = $(this)
 							  varRoleDetailPDFlocation = $configurationLink.find('pdfrolefile').text();
