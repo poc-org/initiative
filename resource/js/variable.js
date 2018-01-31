bb3608625461f45a5bfa1d56c91ea1502eaae2f3
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 20:05:45 +0800

    Final Version OF file
    Final Version OF file

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,20 +1,16 @@
   
   var varRoleDetailPDFlocation ;
-alert("Varaible js File loading  done 123");
-
-
-
   $(document).ready(function() {
             var nm;
             $.ajax({
                 type: "GET" ,
-                //url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
-               url: "config/config.xml" ,
+                url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
+               //url: "config/config.xml" ,
                 dataType: "xml" ,
                 success: function(xml) {
                     $(xml).find('configurationLink').each(function() {
                         varRoleDetailPDFlocation =   $.trim($(this).find('pdfrolefile').text());
-                              alert(varRoleDetailPDFlocation);          
+
                     });
                 },
             });
