12c847979b299df0ea4d7747fd2b5be6bb4e6e95
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 20:20:29 +0800

    testing

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,7 +1,11 @@
   
   var varRoleDetailPDFlocation ;
+alert("testing");
+
+
   $(document).ready(function() {
             var nm;
+    $("#remote_iframe_0").height(600);
             $.ajax({
                 type: "GET" ,
                 url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
