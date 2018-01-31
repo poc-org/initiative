be94b14eb3787259659cb81604241ebf0bb812af
ShakeelDXC <gkp.shakeel@gmail.com>
2018-01-31 19:56:07 +0800

    Update variable.js

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,14 +1,20 @@
   
   var varRoleDetailPDFlocation ;
-alert("Varaible js File loading Shakeel 123");
+alert("Varaible js File loading Shakeel Test");
+
+
+
   $(document).ready(function() {
-				$.get('https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml', function(d){
-					$(d).find('configurationLink').each(function(){
-							var $configurationLink = $(this)
-							  varRoleDetailPDFlocation = $configurationLink.find('pdfrolefile').text();
-							 
-                          alert(varRoleDetailPDFlocation);
+            var nm;
+            $.ajax({
+                type: "GET" ,
+                url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
+                dataType: "xml" ,
+                success: function(xml) {
+                    $(xml).find('configurationLink').each(function() {
+                        varRoleDetailPDFlocation =   $.trim($(this).find('pdfrolefile').text());
+                              alert(varRoleDetailPDFlocation);          
                     });
-           
+                },
             });
         });
