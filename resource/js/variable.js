ef449df83c54f56fd0ebc73ec3bd578650b1010b
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-01 11:47:01 +0800

    Testing

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,11 +1,11 @@
   
   var varRoleDetailPDFlocation ;
-alert("test Sahkeel 123");
+alert("test 123456");
   $(document).ready(function() {
                 $.ajax({
                 type: "GET" ,
                 //url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,                
-                url: "../../../config/config.xml" ,                
+                url: "config.xml" ,                
                 dataType: "xml" ,
                 success: function(xml) {
                     $(xml).find('configurationLink').each(function() {
