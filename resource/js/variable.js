26cf019ab7789868947f9c6e006d5aa3290ac74f
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-01 11:11:05 +0800

    Test

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -1,10 +1,12 @@
   
   var varRoleDetailPDFlocation ;
+var urlPDF = 'config/config.xml';
+alert("Tesring");
   $(document).ready(function() {
                 $.ajax({
                 type: "GET" ,
-                url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
-               //url: pathURL,
+                //url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
+                url: urlPDF,
                 dataType: "xml" ,
                 success: function(xml) {
                     $(xml).find('configurationLink').each(function() {
