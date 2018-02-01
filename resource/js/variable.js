ad144e6e7fe0d3487d966fa79f5b933416169938
ShakeelDXC <gkp.shakeel@gmail.com>
2018-02-01 10:42:47 +0800

    Testing

--- resource/js/variable.js
+++ resource/js/variable.js
@@ -4,11 +4,11 @@
 
 
   $(document).ready(function() {
-    
+        pathURL= "config/config.xml"
             $.ajax({
                 type: "GET" ,
-                url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
-               //url: "config/config.xml" ,
+                //url: "https://raw.githubusercontent.com/poc-org/initiative/itg/resource/config/config.xml" ,
+               url: pathURL,
                 dataType: "xml" ,
                 success: function(xml) {
                     $(xml).find('configurationLink').each(function() {
